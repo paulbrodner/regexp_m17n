908cf8354933c815b1423ecbc0e52c9c111ec653
Work Paul <development1.paul@gmail.com>
2014-08-06 18:08:20 +0300

    Small change

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,6 +1,6 @@
 module RegexpM17N
   def self.non_empty?(str)
     # this will work for bytes encoded in UTF8 - "." in our case
-    str.force_encoding(str.encoding) =~/^.+$/
+    str.force_encoding('utf-8') =~/^.+$/
   end
 end
